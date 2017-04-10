5b7a3d28b1a46ed44de5c28e5af5e0dff65ee749
Manas Solanki <manas@erpnext.com>
2017-04-10 19:50:58 +0530

    Changes in the student group and electives in the program enrollment

--- erpnext/schools/doctype/student_group/student_group.js
+++ erpnext/schools/doctype/student_group/student_group.js
@@ -1,19 +1,32 @@
 cur_frm.add_fetch("student", "title", "student_name");
 
 frappe.ui.form.on("Student Group", {
+
+	onload: function(frm) {
+		frm.set_query("academic_term", function() {
+			return {
+				"filters": {
+					"academic_year": (frm.doc.academic_year)
+				}
+			};
+		});
+		frm.set_value("academic_year", frappe.defaults.get_default("academic_year"))
+		frm.set_value("academic_term", frappe.defaults.get_default("academic_term"))
+	},
+
 	refresh: function(frm) {
 		if (!frm.doc.__islocal) {
 			frm.add_custom_button(__("Course Schedule"), function() {
-				frappe.route_options = {
-					student_group: frm.doc.name
-				}
+				// frappe.route_options = {
+				// 	student_group: frm.doc.name
+				// }
 				frappe.set_route("List", "Course Schedule");
 			});
 
 			frm.add_custom_button(__("Assessment Plan"), function() {
-				frappe.route_options = {
-					student_group: frm.doc.name
-				}
+				// frappe.route_options = {
+				// 	student_group: frm.doc.name
+				// }
 				frappe.set_route("List", "Assessment Plan");
 			});
 			frm.add_custom_button(__("Update Email Group"), function() {
@@ -30,15 +43,15 @@ frappe.ui.form.on("Student Group", {
 			});
 		}
 	},
+	group_based_on: function(frm) {
+		if (frm.doc.group_based_on == "Batch") {
+			frm.doc.course = null;
+		}
+		else if (frm.doc.group_based_on == "Course") {
+			frm.doc.program = null;
+			frm.doc.batch = null;
+		}
 
-	onload: function(frm) {
-		frm.set_query("academic_term", function() {
-			return {
-				"filters": {
-					"academic_year": (frm.doc.academic_year)
-				}
-			};
-		});
 	},
 
 	get_students: function(frm) {
@@ -55,8 +68,3 @@ frappe.ui.form.on("Student Group", {
 	},
 
 });
-
-//If Student Batch is entered, deduce program, academic_year and academic term from it
-cur_frm.add_fetch("student_batch", "program", "program");
-cur_frm.add_fetch("student_batch", "academic_term", "academic_term");
-cur_frm.add_fetch("student_batch", "academic_year", "academic_year");
--- erpnext/schools/doctype/student_group/student_group.py
+++ erpnext/schools/doctype/student_group/student_group.py
@@ -30,27 +30,40 @@ class StudentGroup(Document):
 	'''
 	
 	def validate(self):
+		print "===============>>>>>>>>>> Testing <<<<<<<============"
+		self.validate_mandatory_fields()
 		self.validate_strength()
-		self.validate_student_name()
-		self.validate_name()
-		'''
-		if self.student_batch:
-			self.validate_student_batch()
-		'''
+		# self.validate_student_enrollment()
+		# self.validate_student_name()
+		# self.validate_name()
 		validate_duplicate_student(self.students)
+
+	def validate_mandatory_fields(self):
+		if self.group_based_on == "Course" and not self.course:
+			frappe.throw(_("Mandatory Field - Course"))
+		elif self.group_based_on == "Batch" and (not self.program or not self.batch):
+			frappe.throw(_("Mandatory Field - Program and Batch"))
 
 	def validate_strength(self):
 		if self.max_strength and len(self.students) > self.max_strength:
 			frappe.throw(_("""Cannot enroll more than {0} students for this student group.""").format(self.max_strength))
 
+	def validate_student_enrollment(self):
+		program_enrollment = self.get_program_enrollment()
+		students = [d.student for d in program_enrollment]
+		for d in self.students:
+			if d.student not in students:
+				frappe.throw(_("Student {0} is not enrolled in the given {1}".format(d.student, self.group_based_on)))
+
+	'''
 	def validate_student_name(self):
 		for d in self.students:
 			d.student_name = frappe.db.get_value("Student", d.student, "title")
-	
+		
 	def validate_name(self):
-		if frappe.db.exists("Student Batch", self.name):
+			if frappe.db.exists("Student Batch", self.name):
 			frappe.throw(_("""Student Batch exists with same name"""))
-	'''
+
 	def validate_student_batch(self):
 		student_batch_students = []
 		for d in get_student_batch_students(self.student_batch):
@@ -61,31 +74,34 @@ class StudentGroup(Document):
 	'''
 
 	def get_students(self):
-		if self.group_based_on == "Batch" and not self.program and not self.batch:
-			frappe.throw(_("Program and Batch are mandatory field"))			
-		elif self.group_based_on == "Course" and not self.course:
-			frappe.throw(_("Mandatory field - Course"))
-		else:
-			if self.group_based_on == "Batch":
-				students = frappe.db.sql('''select student, student_name from `tabProgram Enrollment` where academic_year = %s
-					and program = %s and student_batch_name = %s''',(self.academic_year, self.program, self.batch), as_dict=1)
-			elif self.group_based_on == "Course":
-				pass
-				# '''
-				# students = frappe.db.sql("select student, student_name, student_batch_name from \
-				# 	`tabProgram Enrollment` where program = %s and academic_year = %s",(self.program, self.academic_year), as_dict=1)
-				# student_list = [d.student for d in students]
-
-				# inactive_students = frappe.db.sql('''
-				# 	select name as student, title as student_name from `tabStudent` where name in (%s) and enabled = 0''' %
-				# 	', '.join(['%s']*len(student_list)), tuple(student_list), as_dict=1)
-
-				# for student in students:
-				# 	if student.student in [d.student for d in inactive_students]:
-				# 		students.remove(student)
-				# '''
-
+		students = self.get_program_enrollment()
 		if students:
 			return students
+		elif self.group_based_on != "Activity":
+			frappe.throw(_("No students are enrolled in the given {}".format(self.group_based_on)))
 		else:
-			frappe.throw(_("No students Found"))
+			frappe.throw(_("Select students manually for the Activity based Group"))
+
+	def get_program_enrollment(self):
+		if self.group_based_on == "Batch":
+			program_enrollments = frappe.db.sql('''select student, student_name from `tabProgram Enrollment` where academic_year = %s
+				and program = %s and student_batch_name = %s''',(self.academic_year, self.program, self.batch), as_dict=1)
+
+		elif self.group_based_on == "Course":
+			return
+			# '''
+			# students = frappe.db.sql("select student, student_name, student_batch_name from \
+			# 	`tabProgram Enrollment` where program = %s and academic_year = %s",(self.program, self.academic_year), as_dict=1)
+			# student_list = [d.student for d in students]
+
+			# inactive_students = frappe.db.sql('''
+			# 	select name as student, title as student_name from `tabStudent` where name in (%s) and enabled = 0''' %
+			# 	', '.join(['%s']*len(student_list)), tuple(student_list), as_dict=1)
+
+			# for student in students:
+			# 	if student.student in [d.student for d in inactive_students]:
+			# 		students.remove(student)
+			# '''
+		else:
+			return
+		return [d for d in program_enrollments if frappe.db.get_value("Student", d.student, "enabled")]
