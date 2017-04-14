f164af3ad812ab7c6aa1b194a0fbc9989c693986
Manas Solanki <manas@erpnext.com>
2017-04-14 13:16:43 +0530

    Changes in the student group

--- erpnext/schools/doctype/student_group/student_group.js
+++ erpnext/schools/doctype/student_group/student_group.js
@@ -1,19 +1,37 @@
 cur_frm.add_fetch("student", "title", "student_name");
 
 frappe.ui.form.on("Student Group", {
+	onload: function(frm) {
+		frm.set_query("academic_term", function() {
+			return {
+				"filters": {
+					"academic_year": (frm.doc.academic_year)
+				}
+			};
+		});
+	},
+
+	before_save: function(frm) {
+		var name;
+		if (frm.doc.group_based_on == "Course") {
+			name = "Course-" + frm.doc.course + "-" + (frm.doc.academic_term?frm.doc.academic_term:frm.doc.academic_year);
+		} else if (frm.doc.group_based_on == "Batch") {
+			name = "Batch-" + frm.doc.program + "-" + frm.doc.batch + "-" + (frm.doc.academic_term?frm.doc.academic_term:frm.doc.academic_year); 
+		} else if (frm.doc.group_based_on == "Activity") {
+			name = "Activity" + "-" + (frm.doc.academic_term?frm.doc.academic_term:frm.doc.academic_year);
+		}
+		if (!frm.doc.__unnamed) {
+			frm.doc.__newname = name;
+		}
+	},
+
 	refresh: function(frm) {
 		if (!frm.doc.__islocal) {
 			frm.add_custom_button(__("Course Schedule"), function() {
-				frappe.route_options = {
-					student_group: frm.doc.name
-				}
 				frappe.set_route("List", "Course Schedule");
 			});
 
 			frm.add_custom_button(__("Assessment Plan"), function() {
-				frappe.route_options = {
-					student_group: frm.doc.name
-				}
 				frappe.set_route("List", "Assessment Plan");
 			});
 			frm.add_custom_button(__("Update Email Group"), function() {
@@ -30,19 +48,46 @@ frappe.ui.form.on("Student Group", {
 			});
 		}
 	},
+	
+	group_based_on: function(frm) {
+		if (frm.doc.group_based_on == "Batch") {
+			frm.doc.course = null;
+		}
+		else if (frm.doc.group_based_on == "Course") {
+			frm.doc.program = null;
+			frm.doc.batch = null;
+		}
+	},
 
-	onload: function(frm) {
-		frm.set_query("academic_term", function() {
-			return {
-				"filters": {
-					"academic_year": (frm.doc.academic_year)
+	students: function(frm) {
+		console.log("in students");
+	},
+
+	update_students: function(frm) {
+		if (frm.doc.students.length === 0) {
+			frm.doc.next_group_roll_number = 1;
+		}
+		frappe.call({
+			method: "update_students",
+			doc:frm.doc,
+			callback: function(r) {
+				if(r.message) {
+					$.each(r.message, function(i, d) {
+						var s = frm.add_child("students");
+						s.student = d.student;
+						s.student_name = d.student_name;
+						if (d.active === 0) {
+							s.active = 0;
+						}
+						s.group_roll_number = frm.doc.next_group_roll_number;
+						frm.doc.next_group_roll_number += 1;
+					});
+					frm.save();
+				} else {
+					frappe.msgprint(__("Group already updated"))
 				}
-			};
-		});
+			}
+		})
 	}
-});
 
-//If Student Batch is entered, deduce program, academic_year and academic term from it
-cur_frm.add_fetch("student_batch", "program", "program");
-cur_frm.add_fetch("student_batch", "academic_term", "academic_term");
-cur_frm.add_fetch("student_batch", "academic_year", "academic_year");
+});
